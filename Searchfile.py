9594d2f9a39b9bff494eac79c8647192b3972147
maazil3861 <maazshaikh7020@gmail.com>
2020-06-17 19:24:33 +0530

    Add class and function for Requests

--- Searchfile.py
+++
@@ -1,6 +0,0 @@
-import  requests
-
-url = 'https://api.github.com/search/code?q=repo:mritd/dockerfile+filename:dockerfile'
-headers = {'content-type': 'application/json'}
-r = requests.get(url, headers=headers)
-print (r.text)

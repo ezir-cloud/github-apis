7bcdf690e1a26f6441c952f3e941e44a65311585
shaukatmakandar786 <shaukatmakandar786@gmail.com>
2020-06-17 20:20:20 +0530

    To  serach specific repository

--- RepositoryDetails.py
+++ RepositoryDetails.py
@@ -3,6 +3,7 @@ import requests
 class RepositoryDetails:
 
     def search_repository(self):
+
         self.url = 'https://api.github.com/search/repositories?q=java'
         headers = {'content-type': 'application/json'}
         self.r = requests.get(self.url, headers=headers)

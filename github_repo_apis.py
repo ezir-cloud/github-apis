9f0d2749189b51c62f6d1f2e6412aa3c2dce0069
maazil3861 <maazshaikh7020@gmail.com>
2020-06-23 12:06:37 +0530

    remove print statement and object name

--- github_repo_apis.py
+++ github_repo_apis.py
@@ -11,18 +11,16 @@ import os
 import constans
 from constans import Github
 
-Github.BASE_URL, Github.SEARCH, Github.CODE
 class GithubRepoApis:
     base_url = os.path.join(Github.BASE_URL.value, Github.SEARCH.value, Github.CODE.value)
 
-    def get_matched_files_in_repo_by_file_name(self, repo_name, file_name):
-        self.query_string = "?q=repo:{}+filename:{}".format(repo_name, file_name)
+    def get_matched_files_in_repo_by_file_name(self,repo_name,file_name):
+
+        self.query_string ="?q=repo:{}+filename:{}".format(repo_name,file_name)
         self.query_url = "{}{}".format(self.base_url, self.query_string)
-        print(self.query_url)
         headers = {'content-type': 'application/json'}
         self.response = requests.get(self.query_url, headers=headers)
-        # access JSON content using json().
-        self.matched_files = self.response.json()
+        self.matched_files = self.response.json()  # access JSON content using json().
         all_file_details = []
 
         for file_details in self.matched_files["items"]:
@@ -37,10 +35,6 @@ class GithubRepoApis:
             required_file_details["owner"]["id"] = file_details.get("repository").get("owner").get("id")
             required_file_details["owner"]["url"] = file_details.get("repository").get("owner").get("url")
             required_file_details["owner"]["html_url"] = file_details.get("repository").get("owner").get("html_url")
-
-
             all_file_details.append(required_file_details)
             return required_file_details
-Dockerfile = GithubRepoApis()
-result = Dockerfile.get_matched_files_in_repo_by_file_name("mritd/dockerfile", "dockerfile")
-print(result)
+

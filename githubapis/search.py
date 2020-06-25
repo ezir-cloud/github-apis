a4fe16da94aa002db680829621e057b600a1b357
maazil3861 <maazshaikh7020@gmail.com>
2020-06-25 23:21:29 +0530

    Create new package githubapis

--- githubapis/search.py
+++ githubapis/search.py
@@ -2,10 +2,38 @@ import requests
 from githubapis.constants import Github
 
 
+class GithubRepoApis:
+
+    def get_matched_files_in_repo_by_file_name(self, repo_name, file_name):
+        self.target_url = "{BASE_URL}/{SEARCH}/{CODE}".format(BASE_URL=Github.BASE_URL.value,
+                                                              SEARCH=Github.SEARCH.value,
+                                                              CODE=Github.CODE.value)
+        self.query_string = "?q=repo:{}+filename:{}".format(repo_name, file_name)
+        self.query_url = "{}{}".format(self.target_url, self.query_string)
+        headers = {'content-type': 'application/json'}
+        self.response = requests.get(self.query_url, headers=headers)
+        self.matched_files = self.response.json()
+
+        all_file_details = []
+        for file_details in self.matched_files["items"]:
+            required_file_details = dict()
+            required_file_details["name"] = file_details.get("name")
+            required_file_details["path"] = file_details.get("path")
+            required_file_details["url"] = file_details.get("url")
+            required_file_details["git_url"] = file_details.get("git_url")
+            required_file_details["html_url"] = file_details.get("html_url")
+            required_file_details["owner"] = dict()
+            required_file_details["owner"]["login"] = file_details.get("repository").get("owner").get("login")
+            required_file_details["owner"]["id"] = file_details.get("repository").get("owner").get("id")
+            required_file_details["owner"]["url"] = file_details.get("repository").get("owner").get("url")
+            required_file_details["owner"]["html_url"] = file_details.get("repository").get("owner").get("html_url")
+            all_file_details.append(required_file_details)
+        return all_file_details
+
+
 class GitRepositoryApisDetails:
 
     def search_repository_details(self,repo_name):
-
         self.target_url = "{BASE_URL}/{SEARCH}/{REPOSITORIES}".format(BASE_URL=Github.BASE_URL.value,
                                                               SEARCH=Github.SEARCH.value,
                                                               REPOSITORIES=Github.REPOSITORIES.value)
@@ -13,12 +41,10 @@ class GitRepositoryApisDetails:
         self.query_url = "{}{}".format(self.target_url,self.query_string)
         headers = {'content-type': 'application/json'}
         self.response = requests.get(self.query_url, headers=headers)
-        self.response.raise_for_status()
         self.matched_repositories = self.response.json()
 
-        result = []
+        all_repositories_details = []
         for repo in self.matched_repositories["items"]:
-
             repo_details = dict()
             repo_details["id"] = repo.get("id")
             repo_details["name"] = repo.get("name")
@@ -44,8 +70,6 @@ class GitRepositoryApisDetails:
 
             repo_details["forks"] = repo.get("forks")
             repo_details["watchers"] = repo.get("watchers")
-            result.append(repo_details)
+            all_repositories_details.append(repo_details)
 
-        return result
-
-
+        return (all_repositories_details)

9846e6a01db6ab8e7e5107bdb1e64c60d3631e16
Tanveer Alam <tan24alam11@gmail.com>
2020-06-27 12:52:22 +0530

    Github API package manager (#8)
    Packaging implementation to generate whl.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import setuptools
+from setuptools import setup
+
+
+setup(
+    name = 'githubapis',
+    version = '0.0.1',
+    packages =setuptools.find_packages(),
+    url = "https://github.com/ezir-cloud/github-apis",
+    license = '' ,
+    author = 'shaukatmakandar786',
+    author_email = 'shaukatmakandar786@gmail.com',
+    long_description = open('README.txt').read(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires = ["requests==2.23.0"],
+    classifiers=[
+        'Programming Language :: Python :: 3.8'
+    ]
+)
